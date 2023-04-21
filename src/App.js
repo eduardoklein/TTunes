9954efffffb1f73cce4e6dbea5c3b2b66621bb5d
Eduardo Klein <eduardo.klein@live.com>
2023-04-20 22:10:17 -0300

    feat: requisito um concluido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>Commit</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/search" component={ Search } />
+          <Route exact path="/" component={ Login } />
+          <Route path="/pagenotfound" component={ NotFound } />
+          <Redirect to="/pagenotfound" />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 

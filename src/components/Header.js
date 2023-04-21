281fe25061e7bd9e6736629cfd8b0718a8cba975
Eduardo Klein <eduardo.klein@live.com>
2023-04-21 13:47:47 -0300

    feat: quarto requisito concluido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -31,6 +32,9 @@ class Header extends React.Component {
       <header data-testid="header-component">
         {carregando
           ? <p>Carregando...</p> : <h1 data-testid="header-user-name">{ user.name }</h1>}
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
